5c3edb9b5e49c8602c403c070793ee2bf36af26a
orangemug <info@orangemug.co.uk>
2015-02-18 12:07:42 +0000

    Moved to using assert.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
+var assert    = require('assert');
 var _         = require('lodash');
-var chai      = require('chai');
 var decorator = require('../index');
-var expect    = chai.expect;
+
 
 var origData = {
   name: 'Oliver Brooks',
@@ -45,13 +45,13 @@ describe('decorator', function() {
 
       it('should strip out keys which have functions for values', function(done) {
         var out = decorator()(testData);
-        expect(out.ohNoAFunction).to.equal(undefined);
+        assert.equal(out.ohNoAFunction, undefined);
         done();
       });
 
       it('should return undefined if no object is given', function(done) {
         var out = decorator()();
-        expect(out).to.equal(undefined);
+        assert.equal(out, undefined);
         done();
       });
     });
@@ -68,12 +68,12 @@ describe('decorator', function() {
       });
 
       it('should not contain the dontshow key', function(done) {
-        expect(out.dontShow).to.equal(undefined);
+        assert.equal(out.dontShow, undefined);
         done();
       });
 
       it('should not remove any other keys', function(done) {
-        expect(out.name).to.equal(origData.name);
+        assert.equal(out.name, origData.name);
         done();
       });
     });
@@ -89,12 +89,12 @@ describe('decorator', function() {
       });
 
       it('should not contain the dontshow key', function(done) {
-        expect(out.description).to.equal(origData.sillyNameForDescription);
+        assert.equal(out.description, origData.sillyNameForDescription);
         done();
       })
 
       it('should not change other keys', function(done) {
-        expect(out.name).to.equal(origData.name);
+        assert.equal(out.name, origData.name);
         done();
       });
     });
@@ -114,12 +114,12 @@ describe('decorator', function() {
       });
 
       it('should tranform the created at date to a time stamp', function(done) {
-        expect(out.createdAt).to.equal(origData.createdAt.getTime());
+        assert.equal(out.createdAt, origData.createdAt.getTime());
         done();
       });
 
       it('should not change other values', function(done) {
-        expect(out.name).to.equal(origData.name);
+        assert.equal(out.name, origData.name);
         done();
       });
     });
@@ -136,19 +136,19 @@ describe('decorator', function() {
 
       it('should remove type from the nested objects', function(done) {
         out = decorate(testData);
-        expect(out.contactDetails[0]).to.not.have.key('type');
+        assert(!out.contactDetails[0].hasOwnProperty('type'));
         done();
       });
 
       it('should remove functions', function(done) {
         out = decorate(testData);
-        expect(out.otherArray).to.have.length(3);
+        assert.equal(out.otherArray.length, 3);
         done();
       });
 
       it('should remove functions', function(done) {
         out = decorate(testData);
-        expect(out.otherArray).to.have.length(3);
+        assert.equal(out.otherArray.length, 3);
         done();
       });
 
