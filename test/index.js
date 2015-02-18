95b1cdd3fa3185abb5b9d5ce8b741df4cbcfac5e
orangemug <info@orangemug.co.uk>
2015-02-18 12:08:14 +0000

    Removed tests nesting.

--- test/index.js
+++ test/index.js
@@ -32,127 +32,124 @@ var origData = {
 };
 
 describe('decorator', function() {
+  var testData;
 
-  describe('decorate', function() {
-    var testData;
+  beforeEach(function(done) {
+    testData = _.clone(origData);
+    done();
+  });
 
-    beforeEach(function(done) {
-      testData = _.clone(origData);
+  context('Default decorator', function() {
+
+    it('should strip out keys which have functions for values', function(done) {
+      var out = decorator()(testData);
+      assert.equal(out.ohNoAFunction, undefined);
       done();
     });
 
-    context('Default decorator', function() {
+    it('should return undefined if no object is given', function(done) {
+      var out = decorator()();
+      assert.equal(out, undefined);
+      done();
+    });
+  });
 
-      it('should strip out keys which have functions for values', function(done) {
-        var out = decorator()(testData);
-        assert.equal(out.ohNoAFunction, undefined);
-        done();
-      });
+  context('With restricted Keys', function() {
+    var out;
 
-      it('should return undefined if no object is given', function(done) {
-        var out = decorator()();
-        assert.equal(out, undefined);
-        done();
-      });
+    beforeEach(function(done) {
+      out = decorator({
+        restrictedKeys: ['dontShow']
+      })(testData);
+
+      done();
     });
 
-    context('With restricted Keys', function() {
-      var out;
-
-      beforeEach(function(done) {
-        out = decorator({
-          restrictedKeys: ['dontShow']
-        })(testData);
-
-        done();
-      });
-
-      it('should not contain the dontshow key', function(done) {
-        assert.equal(out.dontShow, undefined);
-        done();
-      });
-
-      it('should not remove any other keys', function(done) {
-        assert.equal(out.name, origData.name);
-        done();
-      });
+    it('should not contain the dontshow key', function(done) {
+      assert.equal(out.dontShow, undefined);
+      done();
     });
 
-    context('With Translations', function() {
-      var out;
+    it('should not remove any other keys', function(done) {
+      assert.equal(out.name, origData.name);
+      done();
+    });
+  });
 
-      before(function(done) {
-        out = decorator({
-          translations: {sillyNameForDescription: 'description'}
-        })(testData);
-        done();
-      });
+  context('With Translations', function() {
+    var out;
 
-      it('should not contain the dontshow key', function(done) {
-        assert.equal(out.description, origData.sillyNameForDescription);
-        done();
-      })
-
-      it('should not change other keys', function(done) {
-        assert.equal(out.name, origData.name);
-        done();
-      });
+    before(function(done) {
+      out = decorator({
+        translations: {sillyNameForDescription: 'description'}
+      })(testData);
+      done();
     });
 
-    context('With value transforms', function(done) {
-      var out;
+    it('should not contain the dontshow key', function(done) {
+      assert.equal(out.description, origData.sillyNameForDescription);
+      done();
+    })
 
-      before(function(done) {
-        out = decorator({
-          keyValueTransforms: {
-            createdAt: function(v) {
-              return v.getTime();
-            }
+    it('should not change other keys', function(done) {
+      assert.equal(out.name, origData.name);
+      done();
+    });
+  });
+
+  context('With value transforms', function(done) {
+    var out;
+
+    before(function(done) {
+      out = decorator({
+        keyValueTransforms: {
+          createdAt: function(v) {
+            return v.getTime();
           }
-        })(testData);
-        done();
-      });
-
-      it('should tranform the created at date to a time stamp', function(done) {
-        assert.equal(out.createdAt, origData.createdAt.getTime());
-        done();
-      });
-
-      it('should not change other values', function(done) {
-        assert.equal(out.name, origData.name);
-        done();
-      });
+        }
+      })(testData);
+      done();
     });
 
-    context('nested arrays', function() {
-      var decorate;
-
-      before(function(done) {
-        decorate = decorator({
-          restrictedKeys: ['type']
-        });
-        done();
-      });
-
-      it('should remove type from the nested objects', function(done) {
-        out = decorate(testData);
-        assert(!out.contactDetails[0].hasOwnProperty('type'));
-        done();
-      });
-
-      it('should remove functions', function(done) {
-        out = decorate(testData);
-        assert.equal(out.otherArray.length, 3);
-        done();
-      });
-
-      it('should remove functions', function(done) {
-        out = decorate(testData);
-        assert.equal(out.otherArray.length, 3);
-        done();
-      });
-
+    it('should tranform the created at date to a time stamp', function(done) {
+      assert.equal(out.createdAt, origData.createdAt.getTime());
+      done();
     });
+
+    it('should not change other values', function(done) {
+      assert.equal(out.name, origData.name);
+      done();
+    });
+  });
+
+  context('nested arrays', function() {
+    var decorate;
+
+    before(function(done) {
+      decorate = decorator({
+        restrictedKeys: ['type']
+      });
+      done();
+    });
+
+    it('should remove type from the nested objects', function(done) {
+      out = decorate(testData);
+      assert(!out.contactDetails[0].hasOwnProperty('type'));
+      done();
+    });
+
+    it('should remove functions', function(done) {
+      out = decorate(testData);
+      assert.equal(out.otherArray.length, 3);
+      done();
+    });
+
+    it('should remove functions', function(done) {
+      out = decorate(testData);
+      assert.equal(out.otherArray.length, 3);
+      done();
+    });
+
   });
 });
 
